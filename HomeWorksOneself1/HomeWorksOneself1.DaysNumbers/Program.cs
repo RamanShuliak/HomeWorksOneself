4101ac8ee812ddc8f59c52025da9a6676568f160
RamanShuliak <shuliakrs13@gmail.com>
2022-12-07 21:31:23 +0300

    IndexLast

---
+++ HomeWorksOneself1/HomeWorksOneself1.DaysNumbers/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace HomeWorksOneself1.DaysNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EnterDayNumber();
+
+            
+        }
+        static void EnterDayNumber()
+        {
+            Console.WriteLine("Please, enter the number of the day of week, or 0 if you want to close the program");
+
+            var enteredNumber = Console.ReadLine();
+            var dayNumber = 0;
+
+            if (int.TryParse(enteredNumber, out dayNumber))
+            {
+                PrintNameOfDay(dayNumber);
+            }
+            else
+            {
+                Console.WriteLine($"You enter wrong data. Try again {Environment.NewLine}");
+                EnterDayNumber();
+            }
+        }
+
+        static void PrintNameOfDay(int dayNumber)
+        {
+            switch (dayNumber)
+            {
+                case 1:
+                    Console.WriteLine($"You select Monday {Environment.NewLine}");
+                    EnterDayNumber();
+                    break;
+                case 2:
+                    Console.WriteLine($"You select Tuesday {Environment.NewLine}");
+                    EnterDayNumber();
+                    break;
+                case 3:
+                    Console.WriteLine($"You select Wednesday {Environment.NewLine}");
+                    EnterDayNumber();
+                    break;
+                case 4:
+                    Console.WriteLine($"You select Thursday {Environment.NewLine}");
+                    EnterDayNumber();
+                    break;
+                case 5:
+                    Console.WriteLine($"You select Friday {Environment.NewLine}");
+                    EnterDayNumber();
+                    break;
+                case 6:
+                    Console.WriteLine($"You select Saturday {Environment.NewLine}");
+                    EnterDayNumber();
+                    break;
+                case 7:
+                    Console.WriteLine($"You select Sunday {Environment.NewLine}");
+                    EnterDayNumber();
+                    break;
+                case 0:
+                    Console.WriteLine("End the program");
+                    break;
+                default:
+                    Console.WriteLine($"There are no days with such number {Environment.NewLine}");
+                    EnterDayNumber();
+                    break;
+            }
+        }
+    }
+}
---
+++ HomeWorksOneself1/HomeWorksOneself1.MultiplicationTable/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace HomeWorksOneself1.MultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program give you multiplication table of numbers, " +
+                              $"thats you will enter.{Environment.NewLine}");
+            EnterData();
+        }
+
+        static void EnterData()
+        {
+            var rightNumber = 0.0;
+
+            Console.WriteLine($"Please, enter the number{Environment.NewLine}");
+
+            var number = Console.ReadLine();
+
+            /*var isNumberRight = double.TryParse(number)*/
+
+            if (double.TryParse(number, out rightNumber))
+            {
+                PrintMultiplicationTable(rightNumber);
+            }
+            else
+            {
+                Console.WriteLine($"Entered data is wrong. Try again{Environment.NewLine}");
+
+                ContinueOrNot();
+            }
+        }
+
+        static void ContinueOrNot()
+        {
+            Console.WriteLine($"Do you want yo continue? Y/N{Environment.NewLine}");
+
+            var continueOrNot = Console.ReadLine();
+            if (continueOrNot.ToUpperInvariant() == "Y")
+            {
+                EnterData();
+            }
+            else if (continueOrNot.ToUpperInvariant() == "N")
+            {
+                Console.WriteLine("Program end. Goodby :-)");
+            }
+            else
+            {
+                Console.WriteLine($"Entered data is wrong. Try again{Environment.NewLine}");
+
+                ContinueOrNot();
+            }
+        }
+
+        static void PrintMultiplicationTable(double rightNumber)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{rightNumber} * {i} = {rightNumber * i}");
+            }
+            Console.WriteLine($"{Environment.NewLine}");
+            ContinueOrNot();
+        }
+    }
+}
