24dceb6f511a78cfa8fc4fdc6daa98825e72f410
RamanShuliak <shuliakrs13@gmail.com>
2023-01-31 21:36:32 +0300

    IndexLast

---
+++ HomeWorksOneself4/HomeWorksOneself4.WorkWithFilesAndStrings/ParsingMethods.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorksOneself4.WorkWithFilesAndStrings
+{
+    public class ParsingMethods
+    {
+        public void ParsingForSentences(string filePath)
+        {
+
+            using(var reader = new StreamReader(filePath, Encoding.UTF8))
+            {
+                var text = File.CreateText(@"D:\Programming\Repository\HomeWorksOneself\HomeWorksOneself4\WorkingFiles\Sentences.txt");
+
+                var numberOfSentences = 0;
+
+                while(!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+
+                    var sentences = line.Split('.', '!', '?');
+
+                    foreach(var sentence in sentences)
+                    {
+                        if (sentence.Length >= 2)
+                        {
+                            text.WriteLine($"New sentence - {sentence}");
+
+                            numberOfSentences++;
+                        }
+
+                    }
+                }
+
+                Console.WriteLine($"Number of sentences in text = {numberOfSentences}");
+            }
+        }
+    }
+}
---
+++ HomeWorksOneself4/HomeWorksOneself4.WorkWithFilesAndStrings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+namespace HomeWorksOneself4.WorkWithFilesAndStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var filePath = 
+                @"D:\Programming\Repository\HomeWorksOneself\HomeWorksOneself4\WorkingFiles\SampleForHomeWork4.txt";
+
+            var parsingMethods = new ParsingMethods();
+
+            parsingMethods.ParsingForSentences(filePath);
+        }
+    }
+}
